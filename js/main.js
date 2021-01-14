96fbb83ca430ee6f91ea9084370a9df7b9834ba9
Mohammad Wasiq Ahmadzai <32957037+wasiqkhan@users.noreply.github.com>
2021-01-15 00:40:32 +0430

    added mobile menu

---
+++ js/main.js
@@ -0,0 +1,41 @@
+document.addEventListener('DOMContentLoaded',()=>{
+    document.querySelector('.nav__trigger').addEventListener('click',triggerNav);
+
+
+});
+
+function triggerNav(ev){
+    ev.preventDefault;
+    let nav=document.querySelector('.nav__list');
+    let icon=document.querySelector('#trigger');
+    nav.classList.toggle('show');
+    let isMobileMenu=nav.classList.contains('show');
+    if(isMobileMenu){
+        icon.className='fas fa-times' 
+    }else{
+        icon.className='fa fa-bars';
+       
+    };
+    toggleArrows(nav); 
+}
+
+function toggleArrows(nav){
+    let arrows=document.querySelectorAll('.dropdown-content__link--arrow');
+    arrows.forEach((arrow)=>{
+        if(nav.classList.contains('show')){
+            arrow.className='fa fa-caret-down nav__link--arrow dropdown-content__link--arrow';
+        }else{
+            arrow.className='fa fa-caret-right nav__link--arrow dropdown-content__link--arrow';
+            
+        }
+
+    })
+}
+
+function change(dropdown){
+    let parent=dropdown.parentElement;   //parent of the i which is a
+    parent.nextElementSibling.classList.toggle('show'); //  ul which is the dropdown
+    dropdown.classList.toggle('fa-caret-up');
+    
+
+}
